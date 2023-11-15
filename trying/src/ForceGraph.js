98b6f257a52b0559acd98a8782d221ff66d113ad
Sayali <sayalisc2021@gmail.com>
2023-11-15 19:17:57 +0530

    clcol

--- trying/src/ForceGraph.js
+++ trying/src/ForceGraph.js
@@ -7,17 +7,39 @@ import * as THREE from 'three';
 const ForceGraph = ({data}) => {
   const graphRef = useRef(null);
 
+  const assignClusters = (node) => node.cluster || 0; // Default to 0 if no cluster is assigned
+
+  // Define a function to map clusters to colors
+  const getClusterColor = (cluster) => {
+    const colorMap = {
+         
+      1: '#00FF00',  
+      2: '#0000FF',   
+      3: '#FFFF00',
+      4: '#FF0000',
+      5: '#FFFFFF',
+      6: '#800080',
+
+
+    }
+    return colorMap[cluster] || 'gray'; // Default to gray for unknown clusters
+  };;
+
   return (
     <div ref={graphRef}>
       <ForceGraph3D
         graphData={data}
-        nodeAutoColorBy="id" 
+        // nodeAutoColorBy="id" 
+
+        nodeAutoColorBy={(node) => assignClusters(node)} // Automatically color nodes based on clusters
+      nodeColor={(node) => getClusterColor(assignClusters(node))}
+
         nodeLabel="name"
         linkCurvature={0.25}
         nodeThreeObject={node => {
         
         }}
-        
+
         nodeCanvasObject={(node, ctx, globalScale) => {
         const label = node.name;
         const fontSize = 12 / globalScale;
