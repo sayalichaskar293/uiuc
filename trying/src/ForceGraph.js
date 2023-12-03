9d977c8526b3458f6be8a547c8c26f18d14b9093
Sayali <sayalisc2021@gmail.com>
2023-12-03 12:01:21 +0530

    curved links

--- trying/src/ForceGraph.js
+++ trying/src/ForceGraph.js
@@ -161,8 +161,6 @@ const [linkColor, setLinkColor] = useState(() => {
       
         nodeLabel="name"
         linkCurvature={curvedLinks ? 0.25 : 0} 
-        // nodeVal={(node) => node.size || 7}
-        // linkWidth={(node)=>node.strength}
         linkColor={(link) => {
           const sourceNode = link.source;
           return sourceNode ? getClusterColor(assignClusters(sourceNode)) : 'grey';
@@ -171,11 +169,6 @@ const [linkColor, setLinkColor] = useState(() => {
        
         nodeVal={(node) => (currentItemSize/10)*(node.size) || 7}
         linkWidth={(node)=>(currentLinkSize/10)*(node.strength)}
-        // linkColor={(link) => {
-        //   const sourceNode = link.source;
-        //   return sourceNode ? getClusterColor(assignClusters(sourceNode)) : 'orange';
-        // }}
-        // linkColor={(link) => linkColor[`${link.source.id}-${link.target.id}`]}
         
 
         onNodeHover={handleNodeHover}
